d887adaaf6bb9871796c95c839083e8bd5c0aaef
Polina Cherkasova <polina.c@live.com>
2025-12-31 16:23:57 -0800

    Update scaffold.dart

--- website/lib/shared/framework/scaffold.dart
+++ website/lib/shared/framework/scaffold.dart
@@ -6,7 +6,7 @@ import '../design/border.dart';
 import '../design/theme.dart';
 import 'app_structure.dart';
 
-const _version = 'version 7';
+const _version = 'version 8';
 
 class AppScaffold extends StatefulWidget {
   const AppScaffold(
